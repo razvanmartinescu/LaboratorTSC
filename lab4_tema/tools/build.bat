ab58a0f79a3b49b500c0bfb6ef84e0cb5e4bcb60
razvanmartinescu <shadyfanromania@gmail.com>
2022-05-11 15:29:50 -0700

    ...

---
+++ lab4_tema/tools/build.bat
@@ -0,0 +1,6 @@
+:: Copy 
+::**************************************************************************************
+mkdir                                       ..\sim
+copy /Y run.do                              ..\sim
+copy /Y sources.txt                         ..\sim
+copy /Y wave.do                             ..\sim
