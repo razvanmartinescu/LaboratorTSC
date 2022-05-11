c71b07b0078804be21639dc655d3d8362a09e9b7
razvanmartinescu <shadyfanromania@gmail.com>
2022-05-11 15:37:14 -0700

    tema2

---
+++ lab6_tema/tools/regression_test.bat
@@ -0,0 +1,17 @@
+::========================================================================================
+call clean.bat
+::========================================================================================
+call build.bat
+::========================================================================================
+cd ../sim
+
+call run_test.bat 425
+call run_test.bat 461
+call run_test.bat 976
+call run_test.bat 2577
+call run_test.bat 200
+call run_test.bat 1089
+call run_test.bat 342
+call run_test.bat 3579
+call run_test.bat 8568
+call run_test.bat 2346
